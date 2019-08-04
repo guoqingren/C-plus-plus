db4c1228f7dcbe73bbd456ee27a5d8e5d625d14e
guoqingren <2697839737@qq.com>
2019-08-04 20:47:01 +0800

    Add files via upload

---
+++ helloword/helloworld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+// https://blog.csdn.net/yzpbright/article/details/81515459
+
+using namespace std;
+
+int  main()
+{
+    cout <<"hello Ubuntu world 哈哈哈，终于成功了"<<endl;
+    string str;
+    cin>>str;
+    cout<<str<<endl;
+    return 0;
+
+
+}
